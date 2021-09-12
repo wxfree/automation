6739f82444cfa73636e826028eb9d53542be0ea9
wxfree <383175243@163.com>
2021-09-12 16:48:08 +0800

    更新成类方法

--- tools/classifyMP3.py
+++ tools/classifyMP3.py
@@ -1,43 +1,81 @@
 import shutil
 from mutagen import File
 import os
+import logging
+import datetime
+import time
 # os.mkdir(root_dir + '/文件夹名')正反斜杠好像都行
+# 备份之前要考虑备份文件夹是否存在，如果不存在直接复制黏贴，如果存在就要研究如何删掉这个文件夹os.remove, os.rmdir
+# 使用time模块创建备份文件夹精确到秒就不会出现重复备份文件夹问题了，和谐解决上面的问题
 
-root_dir = r'C:\Users\Administrator\Desktop\music'
-root_dir_bak = r'C:\Users\Administrator\Desktop\musicbak'
-# 切换到根目录
-os.chdir(root_dir)
-# 备份源数据
-shutil.copytree(root_dir, root_dir_bak)
-# 获取数据
-for item in os.listdir():
-    # item是文件名或者文件夹名
-    # 组装所有文件、文件夹的路径
-    path = os.path.join(root_dir, item)
-    # 判断是否是文件，将文件夹过滤，下面的path就是歌曲的完整字符串路径
-    if os.path.isfile(path):
-        # 使用mutagen获取mp3歌手、专辑、歌名信息，专辑信息暂时也没啥用，分类完成后再次根据专辑名细分(暂时无用)
-        # {'TIT2': TIT2(encoding=<Encoding.UTF8: 3>, text=['你的微笑']), 'TPE1': TPE1(encoding=<Encoding.UTF8: 3>, text=['F.I.R.飞儿乐团']), 'TALB': TALB(encoding=<Encoding.UTF8: 3>, text=['F.I.R.飞儿乐团'])}
-        targetFile = File(path)
-        author = targetFile['TPE1'].text[0]
-        title = targetFile['TIT2'].text[0]
-        album = targetFile['TALB'].text[0]
-        # 确保数据存在，否则可能出问题(此处需要找几个得不到信息的mp3试试)
-        if author and title and album:
-            # 拼接歌手名作为文件夹
-            targetPath = root_dir + '\\' + author
-            if os.path.exists(targetPath):
-                # 存在这个文件夹就直接把mp3文件移动到这个文件夹下
-                shutil.move(path, targetPath)
-            else:
-                # 否则就先创建这个文件夹，再移动
-                os.mkdir(targetPath)
-                shutil.move(path, targetPath)
 
-# 将分类好的mp3文件全部丢到一个文件夹下,上面代码的逆操作
-# for root, dirs, files in os.walk(root_dir):
-#     print(root)
-#     if len(files) > 0:
-#         for file in files:
-#             print(file)
-#             shutil.move(root + '\\' + file, root_dir)
+class ClassifyMp3:
+    def __init__(self, root_path, need_backup=False):
+        """
+            :param root_path: MP3所在路径,绝对路径
+            :param need_backup: 是否需要备份
+        """
+        self.rootPath = root_path
+        # 备份数据
+        if need_backup:
+            self.backup()
+        # 切换到根目录
+        os.chdir(self.rootPath)
+
+    def backup(self):
+        """不放心可以先备份rootPath文件夹"""
+        now = datetime.datetime.now()
+        str_time = now.strftime('%Y%m%d%H%M%S')
+        backup_dir = self.rootPath + '备份' + str_time
+        # copytree方法目标路径(backup_dir)不能存在
+        shutil.copytree(self.rootPath, backup_dir)
+
+    def move_file_into_folder(self):
+        """将当前rootPath下的mp3文件移动到相应歌手的文件夹下"""
+        for item in os.listdir():
+            print(item)
+            # mac会有很多.DS_Store文件
+            if item == '.DS_Store':
+                continue
+            music_path = os.path.join(self.rootPath, item)
+            if os.path.isfile(music_path):
+                # 使用mutagen获取mp3歌手、专辑、歌名信息，专辑信息暂时也没啥用，分类完成后再次根据专辑名细分(暂时无用)
+                target_file = File(music_path)
+                if not target_file:
+                    logging.error(f"{music_path}:不存在歌曲信息字段")
+                    continue
+                author = target_file['TPE1'].text[0]
+                title = target_file['TIT2'].text[0]
+                album = target_file['TALB'].text[0]
+                if author and title and album:
+                    # 拼接歌手名作为文件夹
+                    target_path = self.rootPath + '/' + author
+                    logging.info(f"{target_path}")
+                    if os.path.exists(target_path):
+                        # 存在这个文件夹就直接把mp3文件移动到这个文件夹下
+                        shutil.move(music_path, target_path)
+                    else:
+                        # 否则就先创建这个文件夹，再移动
+                        os.mkdir(target_path)
+                        shutil.move(music_path, target_path)
+
+    def move_file_out_folder(self):
+        """将rootPath下的各种mp3文件移动到rootPath下"""
+        for root, dirs, files in os.walk(self.rootPath):
+            print(files)
+            if len(files) > 0:
+                for file in files:
+                    # 只关心MP3文件
+                    if file != '.DS_Store' and file.endswith('.mp3'):
+                        print(file)
+                        shutil.move(root + '/' + file, self.rootPath)
+
+    def set_mp3_info(self, title, author, album):
+        """有些mp3没有歌手、专辑、歌名信息，就手动输入下"""
+        pass
+
+
+root_dir = r'/Users/wangxin/Downloads/music'
+mp3 = ClassifyMp3(root_dir)
+# mp3.move_file_out_folder()
+mp3.move_file_into_folder()
