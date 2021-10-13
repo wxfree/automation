c632e7e2c36999e2ed3b16ea0586e30883bbdb97
wxfree <383175243@163.com>
2021-10-13 10:52:20 +0800

    improvement for game guess

--- tools/guessnumber.py
+++ tools/guessnumber.py
@@ -1,10 +1,13 @@
+import time
 from PySide2.QtWidgets import QApplication, QMainWindow, QPushButton, QPlainTextEdit, QMessageBox, QLineEdit
 from random import randint
 
 
 class Guess:
-    def __init__(self):
-        self.answer = randint(1, 2000)
+    def __init__(self, down=1, up=2000):
+        self.up = up
+        self.down = down
+        self.answer = randint(self.down, self.up)
         self.app = QApplication([])
         self.window = QMainWindow()
         self.window.resize(640, 360)
@@ -32,16 +35,32 @@ class Guess:
             QMessageBox.information(self.window, '提示', f'数字范围在1-2000')
             self.textEdit.clear()
             return
+        elif your_guess > self.up or your_guess < self.down:
+            QMessageBox.information(self.window, '提示', f'数字范围在{self.down}~{self.up}之间')
+            self.textEdit.clear()
+            return
         if your_guess > self.answer:
+            self.up = your_guess
             QMessageBox.information(self.window, '提示', f'{your_guess}太大了')
-            self.logText.appendPlainText(f'{your_guess}太大了')
+            self.logText.appendPlainText(f'{your_guess}太大了,范围{self.down}~{self.up}')
         elif your_guess == self.answer:
             QMessageBox.information(self.window, '提示', f'{your_guess},恭喜您猜对了')
             self.logText.appendPlainText(f'{your_guess},恭喜您猜对了')
+            # 猜对直接重置游戏
+            time.sleep(3)
+            self.init_data()
         else:
+            self.down = your_guess
             QMessageBox.information(self.window, '提示', f'{your_guess}太小了')
-            self.logText.appendPlainText(f'{your_guess}太小了')
+            self.logText.appendPlainText(f'{your_guess}太小了,范围{self.down}~{self.up}')
         self.textEdit.clear()
+
+    def init_data(self):
+        """初始化数据，并清空记录框的内容"""
+        self.up = 2000
+        self.down = 1
+        self.answer = randint(self.down, self.up)
+        self.logText.clear()
 
 
 g = Guess()
