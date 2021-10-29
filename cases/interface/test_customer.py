f4d5ad30c63735e447de56a97394ac3bc9ae20ca
wxfree <383175243@163.com>
2021-10-29 16:08:57 +0800

    更新下

--- cases/interface/test_customer.py
+++ cases/interface/test_customer.py
@@ -5,23 +5,23 @@ import os
 # python -m pytest cases/inter/test_customer.py::TestCustomerInterface::test_interface0003 -sv
 
 
-class TestCustomerInterface:
+class TestCustomerInterface(Util):
     current_path = os.path.dirname(__file__)
     parent_path = os.path.dirname(current_path)
 
-    @pytest.mark.parametrize('args', Util().read_yaml(current_path+'/customer.yaml'))
+    @pytest.mark.parametrize('args', Util.read_yaml(current_path+'/customer.yaml'))
     def test_customer_interface(self, args):
         print(os.path.dirname(__file__))
         allure.dynamic.title(args['name'])
         method = args['request']['method']
         route = args['request']['url']
         data = args['request']['params']
-        resp = Util().send_request(method=method, url=Util().get_real_path(route), data=data, headers=self.get_header())
+        resp = self.send_request(method=method, url=self.get_real_path(route), data=data, headers=self.get_header())
         print(resp.text)
         assert resp.json()['code'] == 200
 
     def get_header(self):
-        return {'X-Access-Token': Util().read_yaml(TestCustomerInterface.parent_path+'/extract.yaml')['token']}
+        return {'X-Access-Token': self.read_yaml(TestCustomerInterface.parent_path+'/extract.yaml')['token']}
     #
     # def test_path(self):
     #     print(os.path.dirname(__file__))
--- cases/run.py
+++ cases/run.py
@@ -1,6 +1,9 @@
 import pytest
-
+import time
+import shutil
 
 if __name__ == '__main__':
     pytest.main()
+    time.sleep(2)
+    shutil.copy('environment.properties', 'reports')
 
